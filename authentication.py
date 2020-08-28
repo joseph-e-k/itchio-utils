b0f7079ff38bb335304b76365b83cc71ad27256e
joseph-e-k <joseph-e-k@users.noreply.github.com>
2020-08-28 15:29:54 +0300

    Move to OOP-y connection objects so we don't have to keep passing around the cookie and slug as parameters

--- authentication.py
+++ authentication.py
@@ -1,13 +1,93 @@
+import dataclasses
+import functools
 import re
 
 import requests
 from lxml import html
 
+from GameInfo import GameInfo
 
 LOGIN_URL = "https://itch.io/login"
+ITCH_WEB_ENCODING = "UTF-8"
+BUNDLE_PAGE_URL_FORMAT = "https://itch.io/bundle/download/{}?page={}"
 
 
-def log_in_and_get_cookie(username, password):
+@dataclasses.dataclass(frozen=True)
+class Connection:
+    username: str
+    cookie: str
+
+    @functools.lru_cache
+    def get_page_html_tree(self, url):
+        response = requests.request(
+            method="GET",
+            url=url,
+            headers={
+                "Cookie": self.cookie
+            }
+        )
+        return html.fromstring(response.content.decode(ITCH_WEB_ENCODING))
+
+    def get_bundle(self, bundle_slug):
+        return BundleConnection(slug=bundle_slug, **dataclasses.asdict(self))
+
+
+@dataclasses.dataclass(frozen=True)
+class BundleConnection(Connection):
+    slug: str
+
+    def get_bundle_page_count(self):
+        first_page_html_tree = self.get_bundle_page_html_tree(1)
+        page_count_node = first_page_html_tree.xpath("//span[@class='pager_label']/a")[0]
+        return int(page_count_node.text)
+
+    def scrape_bundle_page(self, page_number):
+        page_html_tree = self.get_bundle_page_html_tree(page_number)
+        return self.parse_bundle_page(page_html_tree)
+
+    def get_bundle_page_html_tree(self, page_number):
+        return self.get_page_html_tree(BUNDLE_PAGE_URL_FORMAT.format(self.slug, page_number))
+
+    def parse_bundle_page(self, html_tree):
+        game_rows = html_tree.xpath("//div[@class='game_row']")
+        games = []
+
+        for game_row in game_rows:
+            games.append(self.bundle_entry_html_to_game_info(game_row))
+
+        return games
+
+    @staticmethod
+    def bundle_entry_html_to_game_info(html_tree):
+        title_node = html_tree.xpath(".//h2[@class='game_title']/a")[0]
+        summary_node = html_tree.xpath(".//div[@class='meta_row game_short_text']")[0]
+        try:
+            file_count_node = html_tree.xpath(".//div[@class='meta_row file_count']")[0]
+        except IndexError:
+            file_count_node = None
+
+        operating_systems = set()
+        operating_system_nodes = html_tree.xpath(
+            ".//div[@class='meta_row']/span[contains(@title, 'Available for ')]"
+        )
+        for node in operating_system_nodes:
+            operating_systems.add(re.match(r"Available for (\w+)", node.attrib["title"]).group(1))
+
+        if file_count_node is None:
+            file_count = 1
+        else:
+            file_count = int(re.match(r"(\d+) files?", file_count_node.text).group(1))
+
+        return GameInfo(
+            title=title_node.text,
+            url=title_node.attrib['href'],
+            summary=summary_node.text,
+            file_count=file_count,
+            operating_systems=operating_systems
+        )
+
+
+def connect(username, password):
     page_response = requests.get(LOGIN_URL)
     page_html_tree = html.fromstring(page_response.content)
     csrf_token_node = page_html_tree.xpath(".//input[@name='csrf_token']")[0]
@@ -28,5 +108,6 @@ def log_in_and_get_cookie(username, password):
 
     login_response_cookies = login_response.headers["Set-Cookie"]
     login_token = re.search(r"\bitchio=([a-zA-Z0-9%]+);", login_response_cookies).group(1)
+    cookie = f"itchio_token={itchio_token}; itchio={login_token}"
 
-    return f"itchio_token={itchio_token}; itchio={login_token}"
+    return Connection(username, cookie)
--- scrape_bundle.py
+++ scrape_bundle.py
@@ -1,76 +1,16 @@
-import csv
-import functools
-import re
 import argparse
-
-import requests
-from lxml import html
+import csv
 
 from GameInfo import GameInfo
-from authentication import log_in_and_get_cookie
+from authentication import connect
 
 BUNDLE_PAGE_URL_FORMAT = "https://itch.io/bundle/download/{}?page={}"
-ENCODING = "UTF-8"
+OUTPUT_ENCODING = "UTF-8"
 DEFAULT_OUTPUT_PATH = "./bundle_scraping_output.csv"
 
 
-def get_bundle_page_count(bundle_slug, cookie):
-    first_page_html_tree = get_bundle_page_html_tree(bundle_slug, cookie, 1)
-    page_count_node = first_page_html_tree.xpath("//span[@class='pager_label']/a")[0]
-    return int(page_count_node.text)
-
-
-def html_to_game_info(html_tree):
-    title_node = html_tree.xpath(".//h2[@class='game_title']/a")[0]
-    summary_node = html_tree.xpath(".//div[@class='meta_row game_short_text']")[0]
-    try:
-        file_count_node = html_tree.xpath(".//div[@class='meta_row file_count']")[0]
-    except IndexError:
-        file_count_node = None
-
-    operating_systems = set()
-    operating_system_nodes = html_tree.xpath(".//div[@class='meta_row']/span[contains(@title, 'Available for ')]")
-    for node in operating_system_nodes:
-        operating_systems.add(re.match(r"Available for (\w+)", node.attrib["title"]).group(1))
-
-    return GameInfo(
-        title=title_node.text,
-        url=title_node.attrib['href'],
-        summary=summary_node.text,
-        file_count=int(re.match(r"(\d+) files?", file_count_node.text).group(1)) if file_count_node is not None else 1,
-        operating_systems=operating_systems
-    )
-
-
-def scrape_bundle_page(bundle_slug, page_number, cookie):
-    page_html_tree = get_bundle_page_html_tree(bundle_slug, cookie, page_number)
-    return parse_bundle_page(page_html_tree)
-
-
-@functools.lru_cache
-def get_bundle_page_html_tree(bundle_slug, cookie, page_number):
-    response = requests.request(
-        method="GET",
-        url=BUNDLE_PAGE_URL_FORMAT.format(bundle_slug, page_number),
-        headers={
-            "Cookie": cookie
-        }
-    )
-    return html.fromstring(response.content.decode(ENCODING))
-
-
-def parse_bundle_page(html_tree):
-    game_rows = html_tree.xpath("//div[@class='game_row']")
-    games = []
-
-    for game_row in game_rows:
-        games.append(html_to_game_info(game_row))
-
-    return games
-
-
 def dump_game_info(games, path, page_in_bundle):
-    with open(path, "a", newline="", encoding=ENCODING) as output_file:
+    with open(path, "a", newline="", encoding=OUTPUT_ENCODING) as output_file:
         writer = csv.writer(output_file)
         for game in games:
             writer.writerow(game.get_friendly_field_values() + (page_in_bundle,))
@@ -87,20 +27,20 @@ def parse_args():
 
 def main():
     args = parse_args()
-    cookie = log_in_and_get_cookie(args.username, args.password)
+    connection = connect(args.username, args.password).get_bundle(args.slug)
 
     print("Scraping bundle metadata (page count, etc.)")
-    page_count = get_bundle_page_count(args.slug, cookie)
+    page_count = connection.get_bundle_page_count()
 
     # Refresh output file
-    with open(args.output_path, "w", newline="", encoding=ENCODING) as output_file:
+    with open(args.output_path, "w", newline="", encoding=OUTPUT_ENCODING) as output_file:
         writer = csv.writer(output_file)
         writer.writerow(GameInfo.get_friendly_field_names() + ("Page in bundle",))
 
     for page_number in range(1, page_count + 1):
         print(f"Handling page {page_number} / {page_count}...")
         print("... Scraping")
-        games = scrape_bundle_page(args.slug, page_number, cookie)
+        games = connection.scrape_bundle_page(page_number)
         print(f"... Writing to {args.output_path}")
         dump_game_info(games, args.output_path, page_number)
 
