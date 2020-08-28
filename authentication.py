8a8ae6160892e6f3549bedf3b7746144083e29e8
joseph-e-k <joseph-e-k@users.noreply.github.com>
2020-08-28 11:59:56 +0300

    Extract login_and_get_cookie to a separate file, authentication.py.
    Rename login_and_get_cookie to log_in_and_get_cookie (because English is dumb).

---
+++ authentication.py
@@ -0,0 +1,32 @@
+import re
+
+import requests
+from lxml import html
+
+
+LOGIN_URL = "https://itch.io/login"
+
+
+def log_in_and_get_cookie(username, password):
+    page_response = requests.get(LOGIN_URL)
+    page_html_tree = html.fromstring(page_response.content)
+    csrf_token_node = page_html_tree.xpath(".//input[@name='csrf_token']")[0]
+    csrf_token = csrf_token_node.attrib["value"]
+    itchio_token = re.search(r"\bitchio_token=([a-zA-Z0-9%]+);", page_response.headers["Set-Cookie"]).group(1)
+
+    login_response = requests.post(
+        LOGIN_URL,
+        data=dict(
+            username=username,
+            password=password,
+            csrf_token=csrf_token,
+        ),
+        headers={
+            "Cookie": f"itchio_token={itchio_token}",
+        }
+    )
+
+    login_response_cookies = login_response.headers["Set-Cookie"]
+    login_token = re.search(r"\bitchio=([a-zA-Z0-9%]+);", login_response_cookies).group(1)
+
+    return f"itchio_token={itchio_token}; itchio={login_token}"
--- scrape_bundle.py
+++ scrape_bundle.py
@@ -8,8 +8,8 @@ from typing import Set, Optional
 import requests
 from lxml import html
 
+from authentication import log_in_and_get_cookie
 
-LOGIN_URL = "https://itch.io/login"
 BUNDLE_PAGE_URL_FORMAT = "https://itch.io/bundle/download/{}?page={}"
 DEFAULT_OUTPUT_PATH = "./bundle_scraping_output.txt"
 
@@ -51,31 +51,6 @@ def parse_bundle_page(page_text):
     return games
 
 
-def login_and_get_cookie(username, password):
-    page_response = requests.get(LOGIN_URL)
-    page_html_tree = html.fromstring(page_response.content)
-    csrf_token_node = page_html_tree.xpath(".//input[@name='csrf_token']")[0]
-    csrf_token = csrf_token_node.attrib["value"]
-    itchio_token = re.search(r"\bitchio_token=([a-zA-Z0-9%]+);", page_response.headers["Set-Cookie"]).group(1)
-
-    login_response = requests.post(
-        LOGIN_URL,
-        data=dict(
-            username=username,
-            password=password,
-            csrf_token=csrf_token,
-        ),
-        headers={
-            "Cookie": f"itchio_token={itchio_token}",
-        }
-    )
-
-    login_response_cookies = login_response.headers["Set-Cookie"]
-    login_token = re.search(r"\bitchio=([a-zA-Z0-9%]+);", login_response_cookies).group(1)
-
-    return f"itchio_token={itchio_token}; itchio={login_token}"
-
-
 def dump_game_info(games, path):
     with open(path, "w") as output_file:
         for game_info in games:
@@ -100,7 +75,7 @@ def main():
 
     games = []
 
-    cookie = login_and_get_cookie(args.username, args.password)
+    cookie = log_in_and_get_cookie(args.username, args.password)
 
     for page_number in itertools.count(1):
         response = requests.request(
